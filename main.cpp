a6b09ad4b486d564cac685d63bc6afa1a121df52
LIM HEE JIN <xeexin0331@gmail.com>
2024-03-21 17:01:25 +0900

    2차 배열 패턴 찾기

--- main.cpp
+++ main.cpp
@@ -1,36 +1,55 @@
-// 1차원 배열의 패턴 찾기
+// 2차원 배열의 패턴 찾기
 
-//JTBCROKYCD
 
 #include<iostream>
 using namespace std;
 
-char vect[10] = "KBSMBCSBS";
-char arr[3];
+char arr[5][5];
+char input[2][2];
 
-int isPattern(int idx) {
-
-    for (int x = 0; x < 3; x++) {
-        if (vect[x + idx] != arr[x]) {
-            return 0; //한글자라도 다르면 즉시 종료
+void init() {
+    //arr 함수 초기화
+    char val = 'A';
+    for (int y = 0; y < 5; y++) {
+        for (int x = 4; x >= 0; x--) {
+            arr[y][x] = val++;
         }
-        return 1; //찾으면 1
     }
+
+    //input 배열 입력 받기
+    for (int y = 0; y < 2; y++)
+        for (int x = 0; x < 2; x++) {
+            cin >> input[y][x];
+        }
 }
-int main() {
 
-    //입력
-    cin >> arr;
+int isPattern(int dy, int dx) {
 
-    for (int x = 0; x <= 9 - 3; x++) {
-        int ret = isPattern(x);
-        if (ret == 1){
-            cout << "Pattern Found";
-            return 0;//패턴 있으면 즉시 종료
+    for (int y = 0; y < 2; y++) {
+        for (int x = 0; x < 2; x++) {
+            if (arr[dy + y][dx + x] != input[y][x]) {
+                return 0;
+            }
+        }
+    }
+    return 1;
+
+}
+int main(){
+
+    //초기화
+    init();
+
+    for (int y = 0; y <= 5-2; y++) {
+        for (int x = 0; x <= 5-2; x++) {
+            int ret = isPattern(y, x);
+            if (ret == 1) {
+                cout << "인덱스의 위치는 " << y << "," << x;
+                return 0;
+            }
         }
     }
 
-    cout << "Pattern Not Found";
-
+    cout << "NOT FOUND";
     return 0;
 }
