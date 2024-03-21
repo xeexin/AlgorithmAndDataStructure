8a26203e7a0ab7233929b04624a0f18e6c7ee908
LIM HEE JIN <xeexin0331@gmail.com>
2024-03-21 21:16:55 +0900

    몇 층에 있나요?

--- main.cpp
+++ main.cpp
@@ -1,55 +1,38 @@
-// 2차원 배열의 패턴 찾기
-
+// 몇층에 있나요?
 
 #include<iostream>
 using namespace std;
+int arr[9][3]={
+        3,5,1,
+        2,3,6,
+        3,6,1,
+        3,1,1,
+        4,5,6,
+        2,4,5,
+        7,3,4,
+        5,7,2,
+        8,6,4
+};
 
-char arr[5][5];
-char input[2][2];
-
-void init() {
-    //arr 함수 초기화
-    char val = 'A';
-    for (int y = 0; y < 5; y++) {
-        for (int x = 4; x >= 0; x--) {
-            arr[y][x] = val++;
-        }
-    }
-
-    //input 배열 입력 받기
-    for (int y = 0; y < 2; y++)
-        for (int x = 0; x < 2; x++) {
-            cin >> input[y][x];
-        }
-}
-
-int isPattern(int dy, int dx) {
-
-    for (int y = 0; y < 2; y++) {
-        for (int x = 0; x < 2; x++) {
-            if (arr[dy + y][dx + x] != input[y][x]) {
-                return 0;
-            }
+int find(int idx, int target[3]) {
+    for (int x = 0; x < 3; x++) {
+        if (arr[idx][x] != target[x]) {
+            return 0;
         }
     }
     return 1;
-
+    
 }
 int main(){
 
-    //초기화
-    init();
+    int target[3] = {3, 6, 1};
 
-    for (int y = 0; y <= 5-2; y++) {
-        for (int x = 0; x <= 5-2; x++) {
-            int ret = isPattern(y, x);
-            if (ret == 1) {
-                cout << "인덱스의 위치는 " << y << "," << x;
-                return 0;
-            }
+    for (int x = 0; x < 9; x++) {
+        int ret = find(x, target);
+        if (ret == 1) {
+            cout << 9 - x << "층에 거주 중 ";
         }
     }
 
-    cout << "NOT FOUND";
     return 0;
 }
