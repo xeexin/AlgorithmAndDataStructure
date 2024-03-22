1d6356e27e2d9acf7e9943c6f3caa70a393253b5
LIM HEE JIN <xeexin0331@gmail.com>
2024-03-22 17:32:04 +0900

    NOSOL : 반장선거

--- main.cpp
+++ main.cpp
@@ -1,56 +1,41 @@
-// dat 2차 배열
-// (y,x) 좌표 입력 받고 상하좌우+본인의 값이 가장 큰 좌표 값 구하기
-
+//반장선거
+//
 #include<iostream>
 using namespace std;
-int arr[4][4] = {
-        4,7,1,8,
-        4,9,5,9,
-        2,6,1,2,
-        1,5,1,3
-};
-
-int getSum(int yy, int xx){
-    int sum =0;
-
-    //자신, 상, 하, 좌, 우
-    int direct[5][2] = {0, 0,
-                        -1, 0,
-                        1, 0,
-                        0, -1,
-                        0, 1};
-
-
-    for (int t = 0; t < 5; t++) {
-        int dy = yy + direct[t][0];
-        int dx = xx + direct[t][1];
-
-        if(dy <0 || dy > 3 || dx <0 || dx >3  )continue;
-
-        sum += arr[dy][dx];
-    }
-
-    return sum;
-    
-}
 int main()
 {
-    int Max = 0;
-    int Maxy, Maxx;
+    // 반 아이들 투표
+    char classMate[2][4] = {"aaa",
+                            "ddd"};
+    char candidate[4] = {"ace"};
 
-    for (int y = 0; y < 4; y++) {
+    char bucket[200]={0};
+
+    for (int y = 0; y < 2; y++) {
         for (int x = 0; x < 4; x++) {
-            int sum = getSum(y, x);
-            if (sum > Max) {
-                Max = sum;
-                Maxy = y;
-                Maxx = x;
+            bucket[classMate[y][x]]++;
+        }
+    }
+
+    int gikwon=0;
+    int MAX=0;
+    int MaxIdx=0;
+
+    for (int x = 0; x < 200; x++) {
+        if (bucket[x] > MAX) {
+            MAX = bucket[x];
+            MaxIdx = x;
+
+            //기권표
+            if (bucket[x] > 0) {
+                if (x != 'a' || x != 'c' || x != 'e') {
+                    gikwon ++;
+                }
             }
         }
     }
 
-    cout << "최댓값은 " << Max << ". 좌표는 (" << Maxy << "," << Maxx << ") 입니다.";
-    
-    
+    cout << "당선된 사람은 " << (char)MaxIdx<< "이고 표는 " << MAX << "개이며 기권표 갯수는 " << gikwon << "입니다.";
+
     return 0;
 }
