50d5c86b8de021281aab86c58818e73acced9ade
LIM HEE JIN <xeexin0331@gmail.com>
2024-03-22 16:03:13 +0900

    direct 1차원 상하좌우 합 구하기

--- main.cpp
+++ main.cpp
@@ -1,36 +1,35 @@
-// dat
-//brr 배열에 있는 문자가 arr에 몇개 있는지 출력
+// dat 1차 배열
+// (y,x) 좌표 입력 받고 상하좌우 합 구하기
 
 #include<iostream>
 using namespace std;
 int main()
 {
+    int arr[4][4] = {
+            4,7,1,8,
+            4,9,5,9,
+            2,6,1,2,
+            1,5,1,3
+    };
 
-    char arr[10] = "NCT127";
-    char brr[20] = "NCT127BTSRIZE";
-    char bucket[200] = {0};
+    // 상, 하, 좌, 우
+    int directY[4] = {-1, 1, 0, 0};
+    int directX[4] = {0, 0, -1, 1};
 
-    //brr 배열 길이 구하기
-    int len;
-    for (int x = 0; x < 20; x++) {
-        if (brr[x] == NULL) {
-            len=x;
-            break;
-        }
+    //입력
+    int y,x;
+    cin >> y >> x;
+
+    int sum=0;
+
+    for (int t = 0; t < 4; t++) {
+        int dy = y + directY[t];
+        int dx = x + directX[t];
+
+        if(dy<0 || dy>3 || dx<0 || dx>3) continue;
+
+        sum += arr[dy][dx];
     }
 
-    for (int x = 0; x < len; x++) {
-        bucket[brr[x]]++;
-    }
-
-    int cnt = 0;
-    for (int x = 0; x < 6; x++) {
-        if (bucket[arr[x]] > 0) {
-            cnt++;
-        }
-    }
-
-    cout << cnt;
-
-    return 0;
+    cout << sum;
 }
