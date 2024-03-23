d4d1cb26f3f9f42e6994591eda50ac4530446bb2
LIM HEE JIN <xeexin0331@gmail.com>
2024-03-23 15:02:02 +0900

    bfs:중복 가능, 주사위 경우의 수 구하기

--- main.cpp
+++ main.cpp
@@ -1,41 +1,34 @@
-//반장선거
-//
+//bfs
+// [주사위] 숫자 n개 입력 받고 n개의 모든 주사위 수 구하기
 #include<iostream>
 using namespace std;
+int n;
+int path[6];
+
+void abc(int lev){
+
+    //종료 조건
+    if (lev == n) {
+        //출력
+        for (int x = 0; x < n; x++) {
+            cout << path[x] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for (int x = 1; x <= 6; x++) {
+        path[lev] = x;
+        abc(lev + 1);
+        path[lev] = 0;
+    }
+
+}
 int main()
 {
-    // 반 아이들 투표
-    char classMate[2][4] = {"aaa",
-                            "ddd"};
-    char candidate[4] = {"ace"};
+    cin >> n;
+    abc(0);
 
-    char bucket[200]={0};
-
-    for (int y = 0; y < 2; y++) {
-        for (int x = 0; x < 4; x++) {
-            bucket[classMate[y][x]]++;
-        }
-    }
-
-    int gikwon=0;
-    int MAX=0;
-    int MaxIdx=0;
-
-    for (int x = 0; x < 200; x++) {
-        if (bucket[x] > MAX) {
-            MAX = bucket[x];
-            MaxIdx = x;
-
-            //기권표
-            if (bucket[x] > 0) {
-                if (x != 'a' || x != 'c' || x != 'e') {
-                    gikwon ++;
-                }
-            }
-        }
-    }
-
-    cout << "당선된 사람은 " << (char)MaxIdx<< "이고 표는 " << MAX << "개이며 기권표 갯수는 " << gikwon << "입니다.";
 
     return 0;
 }
