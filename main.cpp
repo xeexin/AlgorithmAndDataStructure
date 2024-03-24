59eedddf91e3453171ba033fe98a23e90420cbe0
LIM HEE JIN <xeexin0331@gmail.com>
2024-03-24 21:49:01 +0900

    string : substr(),find()

--- main.cpp
+++ main.cpp
@@ -1,22 +1,23 @@
-/*문자를 숫자로 바꾸기
-        숫자를 문자로 바꾸기*/
-
 #include<iostream>
 #include<string>
-
 using namespace std;
 
 int main()
 {
-    //문자 1234를 정수 1234로 바꾸기
-    string arr = "1234";
-    int t = stoi(arr);
-    cout << t + 10 << endl;
+    string arr = "abcdefghijk";
+    int index = arr.find("f", 0);
+    cout << index << endl;
 
+    // find 메소드 사용시 0(n) 1중 for문의 속도로 찾기
+    // 변수이름.find(찾을문자 또는 문자열, 시작 index)
+    //찾는 문자열에 해당하는 index반환 없으면 -1반환
 
-    //정수 1234를 문자 1234로 바꾸기
-    t = 124142312;
-    string  str= to_string(t);
-    cout << str;
-    return 0;
+    //첫번째 f가 아니라 2번째 f를 찾으려면
+    //arr.find(찾을 것, 시작)
+    int index2 = arr.find('i', index + 1);
+    cout << index2 << endl;
+    
+    //arr.substr(시작, 개수)
+    string result = arr.substr(index + 1, index2 - index - 1);
+    cout << result;
 }
