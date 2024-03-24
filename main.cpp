97e636498496d3f65650acd2f31696689b218461
LIM HEE JIN <xeexin0331@gmail.com>
2024-03-24 22:05:26 +0900

    string : 잘못된 문법

--- main.cpp
+++ main.cpp
@@ -1,31 +1,17 @@
-// 문자(열) 입력 받고, 존재하는지 확인하기
-
 #include<iostream>
 #include<string>
+
 using namespace std;
 
 int main()
 {
-    string arr[3] = {
-            "blackpink",
-            "nct",
-            "aespa"
-    };
+    string s = "super";
+    string a = "man";
 
-    string input;
-    cin >> input;
+    string c = s + a;
+    cout << c;
 
-    int flag = 0;
+    // 불가능한 문법
+    // string c = "super" + "man"
 
-    for (int x = 0; x < 3; x++) {
-        if (arr[x].find(input, 0) != -1) {
-            flag = 1;
-            break;
-        }
-    }
-
-    if(flag) cout << "FOUND";
-    else cout << "NOT FOUND";
-
-    return 0;
 }
