2f30b39428dddacdbb00ae244930b8942f69f4bd
LIM HEE JIN <xeexin0331@gmail.com>
2024-03-24 22:42:24 +0900

    [no sol] parcing

--- main.cpp
+++ main.cpp
@@ -1,28 +1,37 @@
 /*
-JAN[HEY]AKT[HA]BTS[WOW]KT 입력
-[ ]안에 있는 값 출력
+"JAN[[HEY]A[K]]T[HA]]BTS[[W]KT"
+[[ 열리는 대괄호가 2개 이상일 때 안에 파싱
 */
 
 #include<iostream>
 #include<string>
 using namespace std;
-
 int main()
 {
-    string arr = "JAN[HEY]AKT[HA]BTS[WOW]KT";
+    string arr = "JAN[[HEY]A[K]]T[HA]]BTS[[W]KT";
 
     int a = 0;
     int b;
 
     while (1) {
-
         a = arr.find("[", a);
         if(a==-1) break;
-
         b = arr.find("]", a + 1);
-        cout << arr.substr(a + 1, b - a - 1) << endl;
+        if(b==-1)break;
+
+        while (1) {
+            int c= arr.find("[", a + 1);
+            if(c<b) {
+                a = c;
+                cout << arr.substr(a + 1, b - a - 1) << endl;
+                a = b + 1;
+            }
+            else break;
+        }
         a = b + 1;
     }
 
+
     return 0;
 }
+
