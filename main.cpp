518492e170210d2e4144fe1909cf3653dcb14b3d
LIM HEE JIN <xeexin0331@gmail.com>
2024-03-24 21:44:41 +0900

    string : stoi, to_string

--- main.cpp
+++ main.cpp
@@ -1,29 +1,22 @@
-// 중복 X
+/*문자를 숫자로 바꾸기
+        숫자를 문자로 바꾸기*/
 
 #include<iostream>
+#include<string>
+
 using namespace std;
-char arr[5] = "ABCD";
-char path[10];
-int visited[4];
-void abc(int lev){
 
-    if (lev == 3) {
-        cout << path <<endl;
-        return;
-    }
-    for (int x = 0; x < 4; x++) {
-        if(visited[x]==1) continue;
-        visited[x] = 1;
-        path[lev] = arr[x];
-        abc(lev + 1);
-        path[lev] = 0;
-        visited[x] = 0; 
-    }
-
-}
 int main()
 {
-    abc(0);
+    //문자 1234를 정수 1234로 바꾸기
+    string arr = "1234";
+    int t = stoi(arr);
+    cout << t + 10 << endl;
 
+
+    //정수 1234를 문자 1234로 바꾸기
+    t = 124142312;
+    string  str= to_string(t);
+    cout << str;
     return 0;
 }
