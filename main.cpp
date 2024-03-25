7f9208ebe4ea580652015f2a48dbf298ed6c6df5
LIM HEE JIN <xeexin0331@gmail.com>
2024-03-25 20:06:24 +0900

    string, 문자 찾기

--- main.cpp
+++ main.cpp
@@ -1,48 +1,24 @@
 #include<iostream>
-#include<string>
 using namespace std;
+int main()
+{
+// 중간에 abs를 껴 넣어 주세요 abs
+    string arr = "asdlkfjabslksdabsjfabslksdj";
+// abs 문자열이 몇개 있는지 출력해 주세요~
+    string target = "abs";
 
-int findNum(string &arr, int idx){
-
-    for (int x = idx; x < arr.length(); x++) {
-        if (arr[x] >= '0' && arr[x] <= '9') return x;
-    }
-
-    return -1;
-}
-
-int findChar(string &arr, int idx){
-    for (int x = idx; x < arr.length(); x++) {
-        if (!(arr[x] >= '0' && arr[x] <= '9')) {
-            return x;
-        }
-    }
-    return -1;
-}
-int main(){
-    string arr = "ATS47UK53KOR2G";
-
-    int a,b,sum;
-    a = 0;
-    sum = 0;
+    int a = 0;
+    int cnt = 0;
 
     while (1) {
-        //숫자 찾기
-        a = findNum(arr,a);
+        a = arr.find(target, a);
         if(a==-1)break;
-
-        //그 다음 문자 찾기
-        b = findChar(arr, a + 1);
-        if(b==-1)break;
-
-        string ret = arr.substr(a, b - a);
-        int ret2 = stoi(ret) + 5;
-        sum += ret2;
-
-        a = b + 1;
+        a = a + 1;
+        // a += arr.length();
+        cnt++;
     }
 
-    cout << sum;
+    cout << cnt;
 
     return 0;
 }
