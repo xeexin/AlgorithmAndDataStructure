01934fc28da367a60261155e9fcdfc1d27fc847a
LIM HEE JIN <xeexin0331@gmail.com>
2024-03-27 21:57:59 +0900

    이진트리 일차배열 dfs

--- main.cpp
+++ main.cpp
@@ -1,32 +1,26 @@
+//이진트리 : 일차배열 path 출력
 #include<iostream>
 using namespace std;
-char name[6] = { "ABCDE" };
-int map[5][5] = {
-        0,1,1,0,0,
-        0,0,0,1,1,
-        0,0,0,0,0,
-        0,0,0,0,0,
-        0,0,0,0,0,
-};
+char arr[10] = " ABC  DE"; //이진트리 1차 배열
 char path[10];
-void dfs(int now, int lev){
-    cout << path << endl;
 
-    for (int x = 0; x < 5; x++) {
-        if (map[now][x] == 1) {
-            path[lev + 1] = name[x];
-            dfs(x, lev + 1);
-            path[lev + 1] = 0;
-            
-        }
-    }
+void dfs(int now, int lev){
+    //종료 조건
+    if (now >= 10)return; //배열 길이 초과
+    if (arr[now] == ' ' || arr[now] == '\0') return; // 공백이거나 널 일경우
+
+    path[lev + 1] = arr[now * 2];
+    dfs(now * 2, lev + 1);
+
+    path[lev + 1] = arr[now * 2 + 1];
+    dfs(now * 2 + 1, lev + 1);
+
+    path[lev + 1] = 0;
+
 }
 int main()
 {
-
-    path[0] = name[0]; //초기화
-
-    dfs(0, 0);
-
+    path[0] = arr[1]; // 'A'
+    dfs(1, 0);
     return 0;
 }
