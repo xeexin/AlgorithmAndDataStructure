db572787d47d87f5238e93e70ce96bf4eeeb8d87
LIM HEE JIN <xeexin0331@gmail.com>
2024-04-02 21:06:56 +0900

    dfs:시작 도착 노선 갯수

--- main.cpp
+++ main.cpp
@@ -2,39 +2,47 @@
 using namespace std;
 char name[6] = "ABTKG";
 int map[5][5] = {
-	0,1,1,0,0,
-	0,0,1,1,1,
-	0,1,0,1,0,
-	0,0,0,0,0,
-	0,0,0,0,0,
+        0,1,1,0,0,
+        0,0,1,1,1,
+        1,1,0,1,0,
+        0,1,0,0,0,
+        0,0,0,0,0,
 };
-int used[6];
+int used[5];
+int cnt;
 
-void dfs(int now){
-    cout << name[now] << " ";
+void dfs(int now, int edIdx){
+
+    //도착지 도착
+    if (now == edIdx) {
+        cnt++;
+    }
 
     for (int x = 0; x < 5; x++) {
-        if (map[now][x] == 1 && used[x] == 0) {
+        if (used[x] == 0 && map[now][x] == 1) {
             used[x] = 1;
-            dfs(x);
+            dfs(x, edIdx);
+            used[x] = 0;  //갔다 오면 0으로 초기화 해주기
         }
     }
 }
-
 int main()
 {
-	char st;
-	cin >> st;
+    char st, ed;
+    cin >> st >> ed;
 
-    int idx = 0;
+    int stidx, edidx;
+
+    // 시작지와 도착지 인덱스 찾기
     for (int x = 0; x < 5; x++) {
-        if (name[x] == st) {
-            idx = x;
-            break;
-        }
+        if (name[x] == st) stidx = x;
+        if(name[x]==ed) edidx = x;
     }
-    used[idx] = 1;
-    dfs(idx);
 
-	return 0;
+    used[stidx] = 1;
+    dfs(stidx, edidx);
+
+    cout << cnt << "개의 루트가 있음";
+
+    return 0;
 }
