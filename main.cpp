5f2a04a6a4738feaa1fb02016eaf5263c5f2b364
LIM HEE JIN <xeexin0331@gmail.com>
2024-04-09 16:35:11 +0900

    투포인터 : 특정한 합을 가지는 부분 배열의 개수

--- main.cpp
+++ main.cpp
@@ -1,32 +1,30 @@
+//특정한 합을 가지는 부분 배열의 개수
 #include<iostream>
+#include<vector>
 using namespace std;
-// 연속된 m개의 숫자의 합이 최대일 때의 Max값은?
 int main()
 {
-    int arr[11] = { 4,2,1,2,2,1,3,4,0,2,3 };
+    //입력
+    // n:배열의 갯수, m:타겟
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> v(n);
+    for (int x = 0; x < n; x++) cin >> v[x];
 
 
-    int n = 11;
-    int m = 4;
-    int sum = 0;
+    int st = 0, ed = 0, sum = 0, cnt = 0;
+    int target = m;
 
-    for (int x = 0; x < 4; x++) {
-        sum += arr[x];
+    while (1) {
+
+        if(sum >= target) sum -= v[st++]; // 목표 값 보다 커진다면, start 인덱스 값 빼주기
+        else if(ed == n) break; // 목표 값 보다 작은데, ed 인덱스가 끝까지 왔다면 종료
+        else sum += v[ed++]; // 목표 값 보다 작다면, ed idx 값 더해주기
+        if(sum==target) cnt++; // 같다면 카운트 쁠쁠
     }
 
-    int MAX = sum;
-
-    for (int x = 0; x < n - m; x++) {
-        if(MAX < sum) MAX = sum;
-
-        sum += arr[x + m];
-        sum -= arr[x];
-    }
-
-    cout << MAX;
-
-    return 0;
-
+    cout << cnt;
 
     return 0;
 }
