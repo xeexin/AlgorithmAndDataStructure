8640f740801a783d06f09529f35916c9342ccd75
LIM HEE JIN <xeexin0331@gmail.com>
2024-04-11 23:00:54 +0900

    타입별 소트

--- main.cpp
+++ main.cpp
@@ -1,49 +1,28 @@
 #include<iostream>
-#include<vector>
 #include<algorithm>
-
+#include<vector>
+#include<string>
 using namespace std;
-
-bool compare(int f, int b)
-{
-    return f > b;
-}
-
 int main()
 {
-    vector<int>vect = { 3,56,1,2,34,5 };
-    sort(vect.begin(), vect.end()); // 디폴트 less (오름차순)
-    sort(vect.begin(), vect.end(), greater<int>()); // 내림차순
+    //1. sort 범위는 맨끝 다음까지 하기
 
-    sort(vect.begin(), vect.end(),compare); // 내림차순 조건을 직접 부여
-
-    for (int x = 0; x < vect.size(); x++) {
-        cout << vect[x]<<" ";
-    }
+    //2. 배열 아니면 begin(),end()
+    vector<int>v = { 2,67,23,32,1,7 };
+    sort(v.begin(), v.end());
 
 
+    string str = "asdf";
+    sort(str.begin(), str.end());
+
+    //3. 배열일 경우 : idx, idx+n 하기
+    int arr[6]= { 2,67,23,32,1,7 };
+    sort(&arr[0], &arr[6]);
+    sort(arr, arr + 6);
 
 
-
-    /*
-    int len = vect.size();
-    for (int x = 0; x < len; x++) {
-        cout << vect[x] << " ";
-    }
-
-     end() 는 마지막 원소가 아니라 마지막 다음칸..
-    for (auto x = vect.begin(); x != vect.end(); ++x)
-    {
-        cout << *x;
-    }
-
-     auto
-     변수 선언시 반드시 값 초기화 해야 함
-     매개변수 사용 불가 (단 리턴값의 자료형으로 사용은 가능)
-     구조체 맴버변수로 사용 불가능
-     */
-
-
+    string s[3] = { "SBS","KBS","MBC" };
+    sort(s, s + 3);
 
     return 0;
 }
