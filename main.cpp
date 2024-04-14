b02fc6bfb6ad9b19112e1c146076e8172ad57cd4
LIM HEE JIN <xeexin0331@gmail.com>
2024-04-14 16:20:00 +0900

    다중조건 bool compare

--- main.cpp
+++ main.cpp
@@ -1,28 +1,42 @@
 #include<iostream>
-#include<algorithm>
-#include<vector>
-#include<string>
 using namespace std;
+
+struct node {
+    char ch;
+    int num;
+};
+node arr[6] = {
+        {'B',7},
+        {'B',6},
+        {'A',7},
+        {'A',2},
+        {'C',5},
+        {'C',3},
+};
+node ret[6];
+
+bool compare(node f, node b){
+    //1. 문자 내림차순
+    //2. 숫자 오름차순
+
+    if(f.ch > b.ch) return true;
+    if (f.ch < b.ch) return false;
+    else{
+        return f.num < b.num; // 문자가 같다면, 숫자 오름 차순으로 정렬
+    }
+}
 int main()
 {
-    //1. sort 범위는 맨끝 다음까지 하기
-
-    //2. 배열 아니면 begin(),end()
-    vector<int>v = { 2,67,23,32,1,7 };
-    sort(v.begin(), v.end());
-
-
-    string str = "asdf";
-    sort(str.begin(), str.end());
-
-    //3. 배열일 경우 : idx, idx+n 하기
-    int arr[6]= { 2,67,23,32,1,7 };
-    sort(&arr[0], &arr[6]);
-    sort(arr, arr + 6);
+    for (int y = 0; y < 6; y++) {
+        ret[y] = arr[y];
+        for (int x = y; x > 0; x--) {
+            if(compare(ret[x-1], ret[x]) == false) swap(ret[x - 1], ret[x]);
+        }
+    }
 
 
-    string s[3] = { "SBS","KBS","MBC" };
-    sort(s, s + 3);
+    for (int x = 0; x < 6; x++) {
+        cout << ret[x].ch << " " << ret[x].num << endl;
+    }
 
-    return 0;
 }
