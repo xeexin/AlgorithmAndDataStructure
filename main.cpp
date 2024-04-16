1dd4ffa3b218cf1781140b4ca31973dcac155a1f
LIM HEE JIN <xeexin0331@gmail.com>
2024-04-16 13:31:10 +0900

    vector practice

--- main.cpp
+++ main.cpp
@@ -1,50 +1,26 @@
-//tong 에 하나씩 넣을 때, 겹치면 pop_back()해주기 문제
-/*
- 정수 n입력 후
- n개의 정수를 입력받는다.
-
- tong 안에 숫자를 담는데..
- 앞뒤로 같은 숫자가 들어오면 숫자 두개는 사라진다.
-
- tong 안에 남는 숫자는 무엇이 있을까?
-
- 10
- 4 4 7 8 1 6 6 1 8 1
-
- 입력시
- 출력은 7 하고 1 이 됩니다.
-
-*/
-
+//
+//4 7 5 5 1 9 7 하드코딩 후
+//4 또는 5또는 9가 등장 할때 마다 result 배열에 값 넣고 출력하기
+//출력 결과는 4 5 5 9 */
 
 #include<iostream>
 #include <vector>
+
 using namespace std;
 int main(){
+    vector<int> v = {4, 7, 5, 5, 1, 9, 7};
+    vector<int> ret;
 
-    int n;
-    cin >> n;
-
-    vector<int> tong;
-
-    for (int x = 0; x < n; x++) {
-        int i;
-        cin >> i;
-
-        tong.push_back(i);
-
-        int s = tong.size();
-        if(s < 2) continue; // 사이즈가 둘 이상일 때 로직 실행됨
-        if (tong[s - 1] == tong[s - 2]) { //최근 들어온 자신과 그 전에 것이 같은지 비교
-            tong.pop_back();
-            tong.pop_back();
+    for (int x = 0; x < v.size(); x++) {
+        if (v[x] == 4 || v[x] == 5 || v[x] == 9) {
+            ret.push_back(v[x]);
         }
     }
 
-    //출력
-    for (int x = 0; x < tong.size(); x++) {
-        cout << tong[x] << " ";
+    for (int x = 0; x < ret.size(); x++) {
+        cout << ret[x] << " ";
     }
+
 
     return 0;
 }
