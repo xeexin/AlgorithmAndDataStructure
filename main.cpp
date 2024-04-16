8de43a06f2543e88807d0ba4875c9a7d07fb1a1e
LIM HEE JIN <xeexin0331@gmail.com>
2024-04-16 13:44:11 +0900

    vector practice3

--- main.cpp
+++ main.cpp
@@ -1,33 +1,39 @@
-/*8. 문제
-        숫자 하나 입력받고 입력받은 숫자만큼 숫자를 입력받기  ( n번 푸쉬백 또는  (n) 하기)
-그다음 입력받은 숫자들의 합출력
+/*
+9. 문제
 
 3
-1 2 4
+    abcd
+    btskbs
+    pd
 
-출력결과: 7*/
-#include<iostream>
+숫자하나 입력받고 vector에 입력받은 숫자 개수 만큼 문자열 입력받기
+입력받은 후 가장 긴 문자열 출력!
+
+*/
+
+#include <iostream>
 #include<vector>
-
 using namespace std;
-
 int main(){
+
     int n;
     cin >> n;
-
-    vector<int> v(n);
-    int cnt = 0;
+    vector<string> str(n);
+    int MAX = 0;
+    int maxIdx = 0;
 
     for (int x = 0; x < n; x++) {
-        int k;
-        cin >> k;
-        v.push_back(k);
+        cin >> str[x];
     }
-    for (int x = 0; x < v.size(); x++) {
-        cnt += v[x];
+    for (int x = 0; x < n; x++) {
+        if (str[x].length() > MAX) {
+            MAX = str[x].length();
+            maxIdx = x;
+        }
     }
-    cout << cnt;
+
+    cout << str[maxIdx];
+
 
     return 0;
 }
-
