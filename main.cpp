5f43ed67f41cdf6fb1a0f8755d40c69801f03d2f
LIM HEE JIN <xeexin0331@gmail.com>
2024-04-16 14:28:42 +0900

    vector 2차 배열 입력 받기

--- main.cpp
+++ main.cpp
@@ -1,29 +1,31 @@
 /*
-10. 이차배열의 형태
-vector<vector<string>>v;
-
-예를들어 vector<vector<string>>v={{1,2,3},{4,5}};
-[0] 123
-[1] 45
-
-출력해 보세요~
-*/
+5
+0 0 0 1 0
+0 0 1 0 0
+0 0 0 1 1
+1 0 1 0 1
+0 0 1 1 0
+ */
 
 #include<iostream>
 #include<vector>
-#include<string>
 using namespace std;
-vector<vector<int>> v = {{1, 2, 3},
-                         {4, 5}};
+
 int main()
 {
-    for (int y = 0; y < v.size(); y++) {
-        for (int x = 0; x < v[y].size(); x++) {
-            cout << v[y][x];
-        }
-        cout << endl;
-    }
+    int n;
+    cin >> n;
 
+    vector<vector<int>> v;
+
+    for (int y = 0; y < n; y++) {
+        v.push_back({});
+        for (int x = 0; x < n; x++) {
+            int a;
+            cin >> a;
+            v[y].push_back(a);
+        }
+    }
 
     return 0;
 }
