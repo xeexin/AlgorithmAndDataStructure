eac3532c71c7af9ecd7687262cf9ee41fe80beff
LIM HEE JIN <xeexin0331@gmail.com>
2024-04-17 13:36:13 +0900

    charg 특정 기준 정렬

--- main.cpp
+++ main.cpp
@@ -1,42 +1,26 @@
-//  우선순위조건
-//	1. 숫자 내림차순
-//	2. 문자가 오름차순
-#include <iostream>
+#include<iostream>
 #include<algorithm>
-#include <vector>
-
 using namespace std;
 
-struct abc{
-char ch;
-int num;
+// h~o 우선
+// 오름차순
+bool compare(char f, char b) {
+    int aa = 0;
+    int bb = 0;
+
+    if(f>='h' && f <='o') aa = 1;
+    if(b>='h' && b<='o') bb = 1;
+
+    if(aa==1 && bb==0) return 1;
+    if(aa=0 && bb== 1) return 0;
+    return f < b;
 };
+int main()
+{
+    char arr[10] = "btsgood";
 
-abc arr[4] = {
-        {'A',1},
-        {'B',2},
-        {'B',1},
-        {'A',9},
-};
-abc ret[4];
+    sort(arr, arr + 7, compare);
 
-int compare(abc f, abc b){
-    if(f.num > b.num ) return 1;
-    if(f.num < b.num) return 0;
-    return f.ch < b.ch;
-}
-int main(){
-
-    for (int y = 0; y < 4; y++) {
-        ret[y] = arr[y];
-        for (int x = y; x > 0; x--) {
-            if(compare(ret[x-1], ret[x])==0) swap(ret[x-1], ret[x]);
-        }
-    }
-
-    for (int x = 0; x < 4; x++) {
-        cout << ret[x].ch << " " << ret[x].num << endl;
-    }
-
+    cout << arr;
     return 0;
 }
