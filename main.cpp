21443a9d75160fe7e35d1bd826706d2b2bf32ad2
LIM HEE JIN <xeexin0331@gmail.com>
2024-04-17 11:42:02 +0900

    삽입 정렬

--- main.cpp
+++ main.cpp
@@ -1,32 +1,23 @@
 #include<iostream>
 using namespace std;
-struct node {
-    char ch;
-    int num;
-};
-node arr[6] = {
-        {'B',7},
-        {'B',6},
-        {'A',7},
-        {'A',2},
-        {'C',5},
-        {'C',3},
-};
-node ret[6];
-
-// 문자 내림 // 숫자 오름
-bool compare(node f, node b){
-    if(f.ch > b.ch) return 1;
-    if(f.ch<b.ch)return 0;
-    return f.num < b.num;
-}
 int main()
 {
-    for (int x = 0; x < 6; x++) {
-        ret[x] = arr[x];
-        for (int y = x; y > 0; y--) {
-            if(compare(ret[y-1],ret[y])==0) swap(ret[y - 1], ret[y]);
+    int arr[7] = { 4,7,1,3,5,2,9 };
+    int result[7];
+    
+    for (int y = 0; y < 7; y++)
+    {
+        result[y] = arr[y];
+        for (int x = y; x > 0; x--)
+        {
+            if (result[x - 1] > result[x])
+            {
+                swap(result[x - 1], result[x]);
+            }
+            else break;
         }
     }
+    for (int x = 0; x < 9; x++)cout << result[x] << " ";
 
-;}
+    return 0;
+}
