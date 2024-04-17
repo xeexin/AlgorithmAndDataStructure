51dc07c5bcbc53b8a70945c0685f1b54de26d914
LIM HEE JIN <xeexin0331@gmail.com>
2024-04-17 11:49:31 +0900

    정렬 예제 연습1

--- main.cpp
+++ main.cpp
@@ -1,23 +1,42 @@
-#include<iostream>
+//  우선순위조건
+//	1. 숫자 내림차순
+//	2. 문자가 오름차순
+#include <iostream>
+#include<algorithm>
+#include <vector>
+
 using namespace std;
-int main()
-{
-    int arr[7] = { 4,7,1,3,5,2,9 };
-    int result[7];
-    
-    for (int y = 0; y < 7; y++)
-    {
-        result[y] = arr[y];
-        for (int x = y; x > 0; x--)
-        {
-            if (result[x - 1] > result[x])
-            {
-                swap(result[x - 1], result[x]);
-            }
-            else break;
+
+struct abc{
+char ch;
+int num;
+};
+
+abc arr[4] = {
+        {'A',1},
+        {'B',2},
+        {'B',1},
+        {'A',9},
+};
+abc ret[4];
+
+int compare(abc f, abc b){
+    if(f.num > b.num ) return 1;
+    if(f.num < b.num) return 0;
+    return f.ch < b.ch;
+}
+int main(){
+
+    for (int y = 0; y < 4; y++) {
+        ret[y] = arr[y];
+        for (int x = y; x > 0; x--) {
+            if(compare(ret[x-1], ret[x])==0) swap(ret[x-1], ret[x]);
         }
     }
-    for (int x = 0; x < 9; x++)cout << result[x] << " ";
+
+    for (int x = 0; x < 4; x++) {
+        cout << ret[x].ch << " " << ret[x].num << endl;
+    }
 
     return 0;
 }
