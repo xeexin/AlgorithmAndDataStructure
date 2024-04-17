42d6e816bac38d1b62282ea46715406daf6f2f9f
LIM HEE JIN <xeexin0331@gmail.com>
2024-04-17 22:41:10 +0900

    pair

--- main.cpp
+++ main.cpp
@@ -1,46 +1,53 @@
-//최종 몇개의 그룹이 있을까요
+//pair 자료형
+// 두개의 자료형을 하나로 묶는 것
+// 장점 : sort 할 때 first 기준으로 정렬해줌
+//구조체로 써도 가능
 
 #include<iostream>
+#include<utility> //헤더파일 불러와야 함 but algorithm이나 vector 있으면 안써도 됨
+#include<algorithm>
 #include<vector>
 using namespace std;
-vector<string>vect =
-        {
-                "AE",
-                "FE",
-                "CE",
-                "CA",
-                "BD",
-                "DB",
-        };
-
-int arr[200];
-int answer = 6; //처음 그룹은 6개임
-
-char findboss(char member)
-{
-    if (arr[member] == 0) return member;
-    char ret = findboss(arr[member]); // 해당 값을 findboss해주어야 함
-    //arr[member] = ret;
-    return ret;
-
-}
-void setunion(char a, char b)
-{
-    if (findboss(a) == findboss(b)) return;
-    arr[findboss(b)] = findboss(a);
-}
+struct node {
+    int a;
+    int b;
+};
 int main()
 {
-    for (int x = 0; x < vect.size(); x++)
-    {
-        if (findboss(vect[x][0]) != findboss(vect[x][1])) // 보스가 같지 않다면
-        {
-            setunion(vect[x][0], vect[x][1]); //보스를 묶어주고
-            answer--; //그룹 개수 하나 줄어듬!
-        }
-    }
+    node t = { 3,6 }; //구조체
 
-    cout << answer << "개";
+    //1.
+    pair<int, int> t1 = { 3,6 }; //pair 자료형 [앞에 거 : first & 뒤에 거: second]
+
+    //출력
+    cout << t1.first << t1.second;
+    //입력
+    cin >> t1.first >> t1.second;
+    cout << t1.first << t1.second;
+
+
+    //2.
+    pair<int, char>t2;
+    cin >> t2.first >> t2.second;
+
+    //3.
+    pair<int, int>t3;
+    t3={ 3,7 };
+    t3 = make_pair(3, 8);
+
+    //4. 페어 안의 페어도 가능
+    pair < int, pair<int, int>>r;
+    r = { 3,{7,9} };
+    cout << r.second.first; //7
+
+
+    //5.
+    vector<pair<int, char>>vect;
+    vect.push_back({ 3,'T' });
+    vect.push_back(make_pair(7, 'E'));
+    vect.push_back({ 1,'T' });
+
+    sort(vect.begin(), vect.end()); //first 기준으로 sort 1-3-7
 
     return 0;
 }
