eece8afec49da148ab8221f3761d19df4a2c6d37
LIM HEE JIN <xeexin0331@gmail.com>
2024-04-21 14:43:46 +0900

    priority_queue

--- main.cpp
+++ main.cpp
@@ -1,59 +1,24 @@
 #include<iostream>
 #include<vector>
 #include<queue>
-#include<stack>
 using namespace std;
 int arr[8] = { 4,7,1,8,5,3,4,2 };
 
 int main()
-{
-    // queue FIFO
-    queue<int>q;
-    for (int x = 0; x < 8; x++)
-    {
-        q.push(arr[x]);
-    }
-    for (int x = 0; x < 8; x++)
-    {
-        cout<<q.front();   //    47185342
-        q.pop();
-    }
-    cout << endl;
-    stack<int>st;
-    for (int x = 0; x < 8; x++)
-    {
-        st.push(arr[x]);
-    }
-    for (int x = 0; x < 8; x++)
-    {
-        cout << st.top();   //    24835741
-        st.pop();
-    }
-    cout << endl;
-    vector<int>vect;
-    for (int x = 0; x < 8; x++)
-    {
-        vect.push_back(arr[x]);
-    }
-    for (int x = 0; x < 8; x++)
-    {
-        cout << vect.back();    //    24785341
-        vect.pop_back();
-    }
-    cout << endl;
-    priority_queue<int, vector<int>>pq; // maxheap
+{   // 구조체 랑 vector 를 이용하서 만든 stl
+    // max heap 디폴트...
 
-    for (int x = 0; x < 8; x++)
-    {
+    priority_queue<int, vector<int>, greater<int>> pq; //기본 값: 내림차순 -> greater<int> : 오름차순으로
+
+    for (int x = 0; x < 8; x++) {
         pq.push(arr[x]);
     }
-    for (int x = 0; x < 8; x++)
-    {
-        cout << pq.top();   //    87431257
+
+    for (int x = 0; x < 8; x++) {
+        cout << pq.top() << " ";
         pq.pop();
     }
 
 
-    
     return 0;
 }
