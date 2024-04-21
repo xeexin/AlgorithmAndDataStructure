c7daa99828bb96cf0b703ae55cbe266f1939da66
LIM HEE JIN <xeexin0331@gmail.com>
2024-04-21 14:48:31 +0900

    우선순위 비교 연산자: pq

--- main.cpp
+++ main.cpp
@@ -1,21 +1,25 @@
 #include<iostream>
 #include<vector>
 #include<queue>
+#include<stack>
 using namespace std;
 int arr[8] = { 4,7,1,8,5,3,4,2 };
-
+struct compare {
+    bool operator()(int b, int f)
+    {
+        return f < b; //내림차순
+    }
+};
 int main()
-{   // 구조체 랑 vector 를 이용하서 만든 stl
-    // max heap 디폴트...
-
-    priority_queue<int, vector<int>, greater<int>> pq; //기본 값: 내림차순 -> greater<int> : 오름차순으로
-
-    for (int x = 0; x < 8; x++) {
+{
+    priority_queue<int, vector<int>,compare>pq;
+    for (int x = 0; x < 8; x++)
+    {
         pq.push(arr[x]);
     }
-
-    for (int x = 0; x < 8; x++) {
-        cout << pq.top() << " ";
+    for (int x = 0; x < 8; x++)
+    {
+        cout<<pq.top() << " ";
         pq.pop();
     }
 
