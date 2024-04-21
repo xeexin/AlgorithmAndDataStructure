536788d221d5d53afdc160a5d84308db30cd1c1b
LIM HEE JIN <xeexin0331@gmail.com>
2024-04-21 21:05:32 +0900

    Dijkstra

--- main.cpp
+++ main.cpp
@@ -1,28 +1,54 @@
 #include<iostream>
-#include<vector>
-#include<queue>
-#include<stack>
 using namespace std;
-int arr[8] = { 4,7,1,8,5,3,4,2 };
-struct compare {
-    bool operator()(int b, int f)
-    {
-        return f < b; //내림차순
-    }
+char name[6] = "ABCDE";
+const int _ = 999;
+int map[5][5] = {
+        _,1,_,9,3,
+        _,_,7,_,1,
+        _,_,_,1,_,
+        _,_,_,_,_,
+        _,_,1,_,_,
 };
-int main()
+int used[5] = {1};
+int result[5] = {_, 1, _, 9, 3};
+int choice()
 {
-    priority_queue<int, vector<int>,compare>pq;
-    for (int x = 0; x < 8; x++)
-    {
-        pq.push(arr[x]);
+    int MIN = 21e8;
+    int minIdx = 0;
+
+    for (int x = 0; x < 5; x++) {
+        if (used[x] == 0 && result[x] < MIN) {
+            MIN = result[x];
+            minIdx = x;
+        }
     }
-    for (int x = 0; x < 8; x++)
-    {
-        cout<<pq.top() << " ";
-        pq.pop();
+    return minIdx;
+}
+void dijkstra()
+{
+    for (int y = 0; y < 5; y++) {
+        int via = choice();
+        used[via] = 1;
+
+        for (int x = 0; x < 5; x++) {
+            int baro = result[x]; //바로 가는 것
+            int ky = result[via] + map[via][x]; //경유
+
+            if (baro > ky) {
+                result[x] = ky;
+            }
+        }
     }
 
+}
+int main()
+{
+    dijkstra();
+
+    for (int x = 0; x < 5; x++)
+    {
+        cout << result[x] << " ";
+    }
 
     return 0;
 }
