5e272538b49029f29190705dc845ff3b557b7cd5
LIM HEE JIN <xeexin0331@gmail.com>
2024-04-23 19:47:37 +0900

    Parametric Search 1차 배열

--- main.cpp
+++ main.cpp
@@ -1,63 +1,30 @@
-/*
-문제Q
-입력!
-정수 n, m 입력 후
-n개의 정수를 입력 받은 후
-m개의 정수도 입력 받습니다.
-
-문제!
-m개의 정수가 n개의 정수 안에 존재하는지 존재여부 출력
-
-9
-1 4 2 15 6 3 2 -43 1
-5
-20 3 4 -22 -43
-
-출력결과
-XOOXO*/
+//[Parametric Search Algorithm]
 
 #include<iostream>
+#include<string>
 #include<algorithm>
-#include<vector>
 using namespace std;
+string arr = "******____"; //60%
+//string arr = "__________";
+//string arr = "**********";
 int main()
 {
-    //1. 입력
-    int n, m;
-    cin >> n;
-    vector<int>v1(n);
-    for (int x = 0; x < n; x++) cin >> v1[x];
+    int MAX = -1;
 
-    cin >> m;
-    vector<int>v2(m);
-    for (int x = 0; x < m; x++)cin >> v2[x];
+    int st = 0;
+    int ed = arr.size() - 1;
 
+    while (st <= ed) {
+        int mid = (st + ed) / 2;
 
-    //2. 정렬
-    sort(v1.begin(), v1.end());
-
-
-    //3. 탐색
-    for (int x = 0; x < m; x++)
-    {
-        int st = 0;
-        int ed = v1.size() - 1;
-        int flag = 0;
-
-        while (st<=ed)
-        {
-            int mid = (st + ed) / 2;
-
-            if (v1[mid] == v2[x])
-            {
-                flag = 1; break;
-            }
-            if (v1[mid] > v2[x]) ed = mid - 1;
-            if (v1[mid] < v2[x]) st = mid + 1;
+        if (arr[mid] == '*') {
+            MAX = mid;
+            st = mid + 1;
+        } else {
+            ed = mid - 1;
         }
-        if (flag == 1) cout << "O ";
-        else cout << "X ";
     }
 
+    cout << (MAX + 1) * 10 << "%";
     return 0;
 }
