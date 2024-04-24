f993b47c91fb7da8bf5b2204cc3597b5f6ccbe87
LIM HEE JIN <xeexin0331@gmail.com>
2024-04-24 20:47:11 +0900

    dfs : 팀 전투력 min으로 팀 나누기

--- main.cpp
+++ main.cpp
@@ -1,47 +1,58 @@
+/*
+a 50
+b 40
+c 99
+d 5
+e 25
+f 6
+g 37
+
+ 서바이벌 선수와 전투력이 있을때
+a~f를 두 팀으로 만들어서 서바이벌 게임을 하고자 한다.
+두 팀의 전투력 차이가 가장 적게하여 두 팀을 구성한다고 했을때
+두 팀의 최소 전투력 차이는 몇이 되는가?*/
+
 #include<iostream>
-#include<queue>
+#include<cstdlib>
 using namespace std;
-
-struct node {
-    string name;
-    int length;
-};
-
-priority_queue<node>q;
-
-bool operator<(node b, node a)
+//br=7
+//lv=6
+int score[7] = { 50,40,99,5,25,6,37 };
+int teamA[7];
+char path[10];
+int Min = 21e8;
+void dfs(int level, int sum)
 {
-    // 길이 오름차순 정렬
-    return a.length < b.length;
+    int against = 0; // 상대편 점수
+    for (int x = 0; x < 7; x++)
+    {
+        if (teamA[x] == 0)against += score[x]; //A가 아닌 팀의 점수
+    }
+    int gap = abs(sum - against); //절댓값
+    if (gap < Min) {
+        Min = gap;
+    }
+
+    if (level == 6)return; // 팀이 다 짜여지면 리턴;
+
+    for (int x = 0; x < 7; x++)
+    {
+        if (level != 0 && path[level - 1] >= (char) (x + 'A'))  continue; // 이전에 선택된 선수가 존재하고 && 오름차순으로 팀 정렬(중복배열방지)
+
+        path[level] = x + 'A';
+        teamA[x] = 1;
+
+        dfs(level + 1, sum + score[x]);
+
+        teamA[x] = 0;
+        path[level] = 0;
+    }
+
 }
 int main()
 {
-    q.push({ "kevin",10 });
-    q.push({ "jane",2 });
-    q.push({ "john",5 });
-    q.push({ "amy",2 });
-    q.push({ "bob",3 });
-    q.push({ "kate",5 });
 
-
-    while (q.size()!=1) {
-        node a = q.top(); // 첫번째로 가장 작은 값
-        q.pop();
-
-        node b = q.top(); // 두번째로 가장 작은 값
-        q.pop();
-
-        // 두 항목의 이름 중 작은 것을 선택하여 새로운 항목의 이름으로 설정
-        string new_name = a.name;
-        if (new_name > b.name) new_name = b.name;
-
-        // 새로운 항목 생성 및 우선순위 큐에 삽입
-        node neweber = { new_name,a.length + b.length };
-        q.push(neweber);
-    }
-    cout << q.top().name << " " << q.top().length;
-
-
+    dfs(0, 0); // level sum;  레벨과, a팀의 전투력의 합
+    cout << Min;
     return 0;
 }
-
