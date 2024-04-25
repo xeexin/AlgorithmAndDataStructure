1dfe165ca53b6be4302ad231d84b766da51419da
LIM HEE JIN <xeexin0331@gmail.com>
2024-04-25 21:10:39 +0900

    dfs : 내정된 1등

--- main.cpp
+++ main.cpp
@@ -1,27 +1,30 @@
+// T가 1등으로 내정되어 있다. 1~3등의 모든 가능한 경우의 수를 구하여라
 #include<iostream>
+#include<string>
 using namespace std;
-char name[6] = "ABCDE";
+char arr[6] = "ATKBG";
 char path[10];
-void abc(int level,int start) // 0
-{
-    if (level == 3)
-    {
+int used[10];
+
+void dfs(int lev){
+
+    if(lev==3) {
         cout << path << endl;
         return;
     }
-    for (int x = start; x < 5; x++)
-    {
-        path[level] = name[x];
-        abc(level + 1, x + 1);
-        path[level] = 0;
 
-
+    for (int x = 0; x < 5; x++) {
+        if (used[x] == 0) {
+            if(lev >0 && path[0]!='T') continue;
+            used[x] = 1;
+            path[lev] = arr[x];
+            dfs(lev + 1);
+            used[x] = 0;
+            path[lev] = 0;
+        }
     }
 }
-int main()
-{
-    abc(0,0); // level start
-
-
+int main(){
+    dfs(0);
     return 0;
 }
