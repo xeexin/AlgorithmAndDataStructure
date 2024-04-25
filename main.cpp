00b25e0815e0b21c34587ef080dcf71e94898a59
LIM HEE JIN <xeexin0331@gmail.com>
2024-04-25 20:51:26 +0900

    dfs : 중복 X, 순서 중복 X

--- main.cpp
+++ main.cpp
@@ -1,31 +1,27 @@
 #include<iostream>
 using namespace std;
 int path[10];
-int used[10];
-// 3개의 주사위 던졌을 경우 순열-> 한번 나왔던것은 또 나오면 않됨
-void abc(int lev)
-{
+
+// 3개의 주사위 던졌을 경우 << 조합 >> ->  농구팀 예.... 순서 중복 X
+void dfs(int lev, int st){
     if (lev == 3) {
-        for (int x = 0; x < 3; x++) {
-            cout << path[x];
+        for (int x = 0; x < lev; x++) {
+            cout << path[x] << " ";
         }
         cout << endl;
         return;
     }
 
-    for (int x = 1; x <= 6; x++) {
-        if(used[x]==1)continue;
-        used[x] = 1;
+    for (int x = st; x <= 6; x++) {
         path[lev] = x;
-        abc(lev + 1);
+        dfs(lev + 1, x + 1);
         path[lev] = 0;
-        used[x] = 0;
     }
-
 }
 int main()
 {
-    abc(0);
+
+    dfs(0, 1);
 
     return 0;
 }
