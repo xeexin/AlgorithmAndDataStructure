a7a8acf753c9917cc57092d7db4d440d20164526
LIM HEE JIN <xeexin0331@gmail.com>
2024-04-25 20:41:46 +0900

    dfs:중복 불가 순열

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
 using namespace std;
 int path[10];
-// 3개의 주사위 던졌을 경우 중복순열-> 다출력 하기
-void abc(int level)
+int used[10];
+// 3개의 주사위 던졌을 경우 순열-> 한번 나왔던것은 또 나오면 않됨
+void abc(int lev)
 {
-    if (level == 3) {
+    if (lev == 3) {
         for (int x = 0; x < 3; x++) {
             cout << path[x];
         }
@@ -13,10 +14,14 @@ void abc(int level)
     }
 
     for (int x = 1; x <= 6; x++) {
-        path[level] = x;
-        abc(level + 1);
-        path[level]=0;
+        if(used[x]==1)continue;
+        used[x] = 1;
+        path[lev] = x;
+        abc(lev + 1);
+        path[lev] = 0;
+        used[x] = 0;
     }
+
 }
 int main()
 {
