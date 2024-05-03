1eea7ac5d9353e7943e88d26f5a9a6fe15fcd8aa
LIM HEE JIN <xeexin0331@gmail.com>
2024-05-03 22:34:25 +0900

    dfs : 원상복구 rspin, lspin, tover;

--- main.cpp
+++ main.cpp
@@ -1,13 +1,57 @@
 #include<iostream>
-#include<cstring>
+#include<string>
 using namespace std;
+int MIN = 21e8;
+string str = "TSMC";
+string target ="TCMS";
 
-int main()
-{
-    char arr[5] = "asdf";
-    char vect[5];
-    //strcpy(vect, arr);  문자열
-    memcpy(vect, arr, sizeof(arr)); //메모리 블록을 복사합니다. 문자열이나 다른 데이터 형식을 모두 복사할 수 있습니다.
+void rspin(){
+    int temp = str[3];
+    for (int x = 3; x > 0; x--) {
+        str[x] = str[x - 1];
+    }
+    str[0] = temp;
+}
+void lspin(){
+    int temp = str[0];
+    for (int x = 0; x < 3; x++) {
+        str[x] = str[x + 1];
+    }
+    str[3] = temp;
+}
 
+void tover() {
+    char temp[4];
+    for (int y = 0; y < 4; y++) {
+        temp[3 - y] = str[y];
+    }
+    for (int x = 0; x < 4; x++) str[x] = temp[x];
+}
+
+void dfs(int lev){
+    if (str == target) {
+        if (MIN > lev) {
+            MIN = lev;
+            return;
+        }
+    }
+
+    if(lev==5) return; //종료조건
+
+    rspin();
+    dfs(lev + 1);
+    lspin(); //원상복구
+
+    lspin();
+    dfs(lev + 1);
+    rspin(); //원상복구
+
+    tover();
+    dfs(lev+1);
+    tover(); //원상복구
+}
+int main(){
+    dfs(0);
+    cout << MIN;
     return 0;
 }
