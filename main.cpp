4d61b0230c777f4572c05854406109e256c38850
LIM HEE JIN <xeexin0331@gmail.com>
2024-05-06 15:14:43 +0900

    우선순위 큐 : 기본

--- main.cpp
+++ main.cpp
@@ -1,42 +1,34 @@
-//n 입력후 , n개의 정수 입력 받기
-// ex) 4 9 3 2 7
-//각 숫자에 3을 곱하거나 2로 나누거나 5를 빼서 숫자를 만든다
-//만들어진 4개의 숫자의 곱이 max일 때의 값 구하기
-
 #include<iostream>
-#include<vector>
+#include <queue>
 using namespace std;
-int n;
-vector<int> vect;
-int MAX = -21e8;
-int calcul[3] = {3, 2, -5};
-
-void dfs(int lev, int sum){
-    if (lev == n) {
-        if (MAX < sum) {
-            MAX = sum;
-        }
-        return;
-    }
-
-    for (int t = 0; t < 3; t++) {
-        if(t==0) dfs(lev + 1, sum * vect[lev] * calcul[0]);
-        if(t==1) dfs(lev + 1, sum * vect[lev] / calcul[1]);
-        if(t==2) dfs(lev + 1, sum * vect[lev] + calcul[2]);
-    }
-
+int arr[6] = { 5,4,2,7,6,3 };
+struct node {
+    int a;
+};
+bool operator<(node b, node a){
+    if (a.a % 2 == 0 && b.a % 2 == 1)return true;
+    if (a.a % 2 == 1 && b.a % 2 == 0) return false;
+    return a.a > b.a;
 }
+// 짝수 우선, 내림차순
 int main(){
 
-    cin >> n;
-    for (int x = 0; x < n; x++) {
-        int t;
-        cin >> t;
-        vect.push_back(t);
+    node b[6];
+
+    for (int x = 0; x < 6; x++) {
+        b[x].a = arr[x];
     }
 
-    dfs(0, 1);
+    priority_queue<node>q;
 
-    cout << MAX;
+    for (int x = 0; x < 6; x++) {
+        q.push(b[x]);
+    }
+
+    for (int x = 0; x < 6; x++) {
+        cout << q.top().a << " ";
+        q.pop();
+    }
+
     return 0;
 }
