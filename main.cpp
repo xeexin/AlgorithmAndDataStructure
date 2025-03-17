c228025b7b1af4a100694dc43a7939410d69186c
Lim Hee Jin <xeexin0331@gmail.com>
2025-03-17 23:05:33 +0900

    백준 2606번

--- main.cpp
+++ main.cpp
@@ -1,58 +1,41 @@
-//백준 1260번
+//백준 2606번
 #include<iostream>
 #include<queue>
 using namespace std;
-int map[1001][1001];
-int visited[1001];
-int n,m,v;
-void reset(){
-    for (int x = 1; x <= n; x++)  visited[x] = 0;
-}
-void dfs(int v){
-    visited[v]=1;
-    cout << v << " ";
-
-    for (int x = 1; x <= n; x++) {
-        if(visited[x]==0 && map[v][x]==1) dfs(x);
-        if(x==n) return;
-    }
-
-}
-void bfs(int v){
-    queue<int>q;
-    q.push(v);
-    visited[v] = 1;
-
-    while (!q.empty()) {
-        int now = q.front();
-        cout << now << " ";
-        q.pop();
-
-        for (int x = 1; x <= n; x++) {
-            if(map[now][x]==1 && visited[x]==0) {
-                q.push(x);
-                visited[x] = 1;
-            }
-        }
-    }
-}
-
+int map[101][101];
+int visited[101];
+int cnt;
 int main(){
-    cin >> n >> m >> v;
+
+    int n,m;
+    cin >> n >> m;
 
     for (int x = 0; x < m; x++) {
         int a,b;
         cin >> a >> b;
-
-        map[a][b] = 1;
+        map[a][b]=1;
         map[b][a] = 1;
     }
 
-    dfs(v);
-    cout << "\n";
-    reset();
-    bfs(v);
+    visited[1] = 1;
+    queue<int>q;
+    q.push(1);
 
+    while (!q.empty()) {
+        int now = q.front();
+        q.pop();
+
+        for (int x = 1; x <= n; x++) {
+            if(visited[x]== 1) continue;
+            if(map[now][x]==1) {
+                q.push(x);
+                visited[x] = 1;
+                cnt++;
+            }
+        }
+    }
+
+    cout << cnt;
 
     return 0;
 }
