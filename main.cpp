295a0c641e41ec02cf2921c02699ec34ba1c371a
Lim Hee Jin <xeexin0331@gmail.com>
2025-03-19 21:31:35 +0900

    백준 13549

--- main.cpp
+++ main.cpp
@@ -1,77 +1,36 @@
-//백준 7576번
+// 백준 13549
 
 #include<iostream>
-#include <queue>
+#include<queue>
 using namespace std;
-int map[1001][1001];
-int visited[1001][1001];
-int m,n;
-struct node{
-    int y,x,lev;
-};
-node now;
-int direct[4][2] = {-1, 0, 1, 0, 0, -1, 0, 1};
-
-int check(){
-    int cnt = 0;
-    for (int y = 0; y < n; y++) {
-        for (int x = 0; x < m; x++) {
-            if(map[y][x]==0) cnt++; //안 익은 토마토 개수
-        }
-    }
-
-    return cnt;
-}
 int main(){
 
-    cin >> m >> n;
+    int n,k;
+    cin >> n >> k;
 
-    for (int y = 0; y < n; y++) {
-        for (int x = 0; x < m; x++) {
-            int a;
-            cin >> a;
-            map[y][x] = a;
+    int visited[100001] = {0};
+    priority_queue<pair<int,int>,vector<pair<int,int>>, greater<pair<int,int>> >pq; // 0초를 고려하여 우선순위 큐에 넣음, 오름차순으로 정렬
+
+
+    pq.push(make_pair(0, n)); //시간 오름차순으로 큐에 넣어야함으로 first는 시간이 들어가야함
+
+    while (!pq.empty()) {
+        int t = pq.top().first;
+        int idx = pq.top().second;
+        pq.pop();
+
+        visited[idx] = 1;
+
+        if (idx == k) {
+            cout << t;
+            break;
         }
+
+        if(idx-1 >=0 && visited[idx-1]==0) pq.push(make_pair(t + 1, idx - 1));
+        if (idx + 1 <= 100000 && visited[idx + 1] == 0) pq.push(make_pair(t + 1, idx + 1));
+        if (idx * 2 <= 100000 && visited[idx * 2] == 0) pq.push(make_pair(t, idx * 2));
     }
 
-    queue<node>q;
-
-    for (int y = 0; y < n; y++) {
-        for (int x = 0; x < m; x++) {
-            if (map[y][x] == 1){
-                q.push({y, x, 0});
-                visited[y][x] = 1;
-            }
-        }
-    }
-
-    int ret = check();
-
-    if(ret ==0) {
-        cout << 0;
-        return 0;
-    }
-
-    while (!q.empty()) {
-        now = q.front();
-        q.pop();
-
-        for (int t = 0; t < 4; t++) {
-            int dy = now.y + direct[t][0];
-            int dx = now.x + direct[t][1];
-
-            if(dy < 0 || dx<0 || dy > n-1 || dx > m-1) continue; //범위 밖
-            if(visited[dy][dx]==1)continue; // 중복
-            if(map[dy][dx]==-1) continue; // 벽
-            visited[dy][dx] = 1;
-            map[dy][dx] = 1;
-            q.push({dy, dx, now.lev + 1});
-        }
-    }
-
-    ret = check();
-    if(ret > 0) cout << -1;
-    else cout << now.lev;
 
     return 0;
 }
