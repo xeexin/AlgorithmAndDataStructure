a0460a5e1e0cec4f683ae7efe48a465fb6186889
Lim Hee Jin <xeexin0331@gmail.com>
2025-03-22 23:01:20 +0900

    백준 1780

--- main.cpp
+++ main.cpp
@@ -1,93 +1,44 @@
-// 백준 5427
+// 백준 1780
 
 #include<iostream>
-#include<queue>
 using namespace std;
-char map[1002][1002];
-int direct[4][2] = {-1, 0, 1, 0, 0, -1, 0, 1};
-int tn,h,w; // 테스트케이스 개수, 높이, 너비
-queue<pair<int,int>> sq; //상근이 위치 큐
-queue<pair<int,int>> fq; //불 위치 큐
-void movef(){
-
-    int size = fq.size();
-
-    for (int x = 0; x < size; x++) {
-        pair<int, int> now = fq.front();
-        fq.pop();
-
-        for (int t = 0; t < 4; t++) {
-            int dy = now.first + direct[t][0];
-            int dx = now.second + direct[t][1];
-
-            if(dy < 0 || dx < 0 || dy > h-1 || dx > w-1) continue; //범위 밖
-            if(map[dy][dx] != '.') continue; // 빈 공간이 아니면 컨티뉴
-            map[dy][dx] = '*'; //불이 번짐
-            fq.push({dy, dx});
-        }
-    }
-}
-int moves(){
-    int time = 0;
-
-    while (!sq.empty()) {
-        time++;
-        movef(); // 불 bfs (1초뒤 불이 번지는 곳으로 못가니까 불 먼저 퍼져야함)
-
-        int size = sq.size();
-        for (int x = 0; x < size; x++) {
-            pair<int, int> now = sq.front();
-            sq.pop();
-
-            for (int t = 0; t < 4; t++) {
-                int dy = now.first + direct[t][0];
-                int dx = now.second + direct[t][1];
-
-                if (dy < 0 || dx < 0 || dy > h - 1 || dx > w - 1) return time; // 범위 밖 = 출구
-                if(map[dy][dx] != '.') continue;
-
-                map[dy][dx] = '@';
-                sq.push({dy, dx});
+int cnt_1, cnt0, cnt1;
+int arr[2200][2200];
+bool issame(int yy, int xx, int n){
+    int target = arr[yy][xx];
+    for (int y = yy; y < yy + n; y++) {
+        for (int x = xx; x < xx + n; x++) {
+            if (target != arr[y][x]) {
+                return false;
             }
         }
     }
-    return -1;
+    return true;
 }
-
-void reset(int w, int h){
-    while(!sq.empty()) sq.pop();
-    while(!fq.empty()) fq.pop();
-
-    for (int y = 0; y < h; y++) {
-        for (int x = 0; x < w; x++) {
-            map[y][x] = '.';
+void div(int yy, int xx, int n) {
+    if (issame(yy, xx, n)) {
+        if (arr[yy][xx] == -1) cnt_1++;
+        if (arr[yy][xx] == 0) cnt0++;
+        if (arr[yy][xx] == 1) cnt1++;
+    }else{
+        int volume = n / 3;
+        for (int y = 0; y < 3; y++) {
+            for (int x = 0; x < 3; x++) {
+                div(volume * y+yy, volume * x+xx, volume);
+            }
         }
     }
 }
 int main(){
-
-    cin >> tn;
-
-    for (int x = 0; x < tn; x++) {
-        cin >> w >> h;
-
-        reset(w, h);
-
-        for (int y = 0; y < h; y++) {
-            for (int x = 0; x < w; x++) {
-                cin >> map[y][x];
-
-                if(map[y][x]=='@') sq.push({y, x}); //상근이 위치 큐에 넣기
-                if(map[y][x]=='*') fq.push({y, x}); //불 위치 큐에 넣기
-            }
+    int n;
+    cin >> n;
+    for (int y = 0; y < n; y++) {
+        for (int x = 0; x < n; x++) {
+            cin >> arr[y][x];
         }
-
-        int ret = moves(); //상근이 움직이는 bfs
-
-        if (ret == -1) {
-            cout << "IMPOSSIBLE" << endl;
-        } else cout << ret << endl;
     }
 
+    div(0, 0, n);
+    cout << cnt_1 << endl << cnt0 << endl << cnt1;
     return 0;
 }
