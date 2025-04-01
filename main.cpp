a13d3bf5385fd38538578b7f9ca50cf844e66045
HEEJIN LIM <xeexin0331@gmail.com>
2025-04-01 18:26:06 +0900

    S_1_14889

--- main.cpp
+++ main.cpp
@@ -1,23 +1,50 @@
-// 백준 G_5_11729
+// 백준 S_1_14889
 
 #include <iostream>
+#include<cstdlib>
+using namespace std;
+int n;
+int MIN = 21e8;
+int arr[21][21];
+bool check[21];
 
-void hanoi(int n, int from, int by, int to) {
-    if (n == 1) { std::cout << from << ' ' << to << '\n'; } else {
-        hanoi(n - 1, from, to, by);
-        std::cout << from << ' ' << to << '\n';
-        hanoi(n - 1, by, from, to);
+void dfs(int lev, int st) {
+    if (lev == (n / 2)) {
+        int teamStart, teamLink;
+        teamLink = 0;
+        teamStart = 0;
+
+        for (int y=0; y<n; y++) {
+            for (int x=0; x<n; x++) {
+                if (check[y] == true && check[x]== true) teamStart+=arr[y][x];
+                if (check[y] == false && check[x] == false) teamLink+=arr[y][x];
+            }
+        }
+
+        int gap = abs(teamLink - teamStart);
+        if (gap < MIN) MIN = gap;
+        return;
     }
-}
 
-int hanoiCnt(int n) {
-    if (n == 1) return 1;
-    return 2 * hanoiCnt(n - 1) + 1;
+    for (int x=st; x<n; x++) {
+        check[x] = true;
+        dfs(lev+1, x+1);
+        check[x] = false;
+    }
+
 }
 
 int main() {
-    int n;
-    std::cin >> n;
-    std::cout << hanoiCnt(n) << '\n';
-    hanoi(n, 1, 2, 3);
+    //1. 입력
+    cin >> n;
+
+    for (int y = 0; y < n; y++) {
+        for (int x = 0; x < n; x++) {
+            cin >> arr[y][x];
+        }
+    }
+
+    dfs(0, 0);
+
+    cout << MIN << endl;
 }
