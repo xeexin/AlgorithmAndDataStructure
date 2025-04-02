30889122e39890fe89ce61d531395eda61784b99
HEEJIN LIM <xeexin0331@gmail.com>
2025-04-02 17:10:46 +0900

    S_1_14888

--- main.cpp
+++ main.cpp
@@ -1,50 +1,59 @@
-// 백준 S_1_14889
+// 백준 S_1_14888
 
 #include <iostream>
-#include<cstdlib>
 using namespace std;
+int arr[12]; //순서 바뀌면 안됨.
+int sam[4];
 int n;
+int MAX = -21e8;
 int MIN = 21e8;
-int arr[21][21];
-bool check[21];
 
-void dfs(int lev, int st) {
-    if (lev == (n / 2)) {
-        int teamStart, teamLink;
-        teamLink = 0;
-        teamStart = 0;
-
-        for (int y=0; y<n; y++) {
-            for (int x=0; x<n; x++) {
-                if (check[y] == true && check[x]== true) teamStart+=arr[y][x];
-                if (check[y] == false && check[x] == false) teamLink+=arr[y][x];
-            }
-        }
-
-        int gap = abs(teamLink - teamStart);
-        if (gap < MIN) MIN = gap;
+void dfs(int lev, int sum) {
+    if (lev == n) {
+        if (sum > MAX) MAX = sum;
+        if (sum < MIN) MIN = sum;
         return;
     }
-
-    for (int x=st; x<n; x++) {
-        check[x] = true;
-        dfs(lev+1, x+1);
-        check[x] = false;
+    for (int x = 0; x < 4; x++) {
+        if (sam[x] > 0) {
+            sam[x]--;
+            if (x == 0) {
+                // 덧셈
+                dfs(lev + 1, sum + arr[lev]);
+            } else if (x == 1) {
+                // 뺄셈
+                dfs(lev + 1, sum - arr[lev]);
+            } else if (x == 2) {
+                // 곱셈
+                dfs(lev + 1, sum * arr[lev]);
+            } else if (x == 3) {
+                // 나눗셈
+                int temp;
+                if (sum < 0)
+                    temp = -(-sum / arr[lev]);
+                else
+                    temp = sum / arr[lev];
+                dfs(lev + 1, temp);
+            }
+            sam[x]++;
+        }
     }
-
 }
 
 int main() {
-    //1. 입력
     cin >> n;
-
-    for (int y = 0; y < n; y++) {
-        for (int x = 0; x < n; x++) {
-            cin >> arr[y][x];
-        }
+    for (int x = 0; x < n; x++) {
+        cin >> arr[x];
     }
 
-    dfs(0, 0);
+    for (int x = 0; x < 4; x++) {
+        cin >> sam[x];
+    }
 
-    cout << MIN << endl;
+    dfs(1, arr[0]);
+
+    cout << MAX << endl << MIN << endl;
+
+
+    return 0;
 }
