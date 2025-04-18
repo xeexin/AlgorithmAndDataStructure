f1719998d39d08a8d903789d4256aa6c5c3343ad
HEEJIN LIM <xeexin0331@gmail.com>
2025-04-18 16:24:12 +0900

    S_4_1015

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
-// 백준 S_5_5648
+// 백준 S_4_1015
 
-/**
-모든 원소가 양의 정수인 집합이 있을 때, 원소를 거꾸로 뒤집고 그 원소를 오름차순으로 정렬하는 프로그램을 작성하세요.
-단, 원소를 뒤집었을 때 0이 앞에 선행되는 경우는 0을 생략해야합니다.
+/*
+
+A[0]=2=B[P[0]]
+A[1]=3=B[P[1]]
+A[2]=1=B[P[2]]
+
+B[P[2]]=1=B[0]
+B[P[0]]=2=B[1]
+B[P[1]]=3=B[2]
+
+P[2]=0
+P[0]=1
+P[1]=2
+
+
+p = {1,2,0}
 */
 
 #include <iostream>
@@ -11,20 +24,22 @@
 using namespace std;
 int main() {
     int n;
-    cin >> n;
-    vector<long long> v;
-
-    for (int x = 0; x < n; x++) {
-        string num;
-        cin >> num;
-        reverse(num.begin(), num.end());
-        long long reverseNum = stoll(num);
-        v.push_back(reverseNum);
+    cin>>n;
+    vector<pair<int, int>> a;
+    for (int x=0; x<n; x++) {
+        int num; cin>>num;
+        a.push_back(make_pair(num, x)); // {{2,0}, {3,1}, {1,2}}
     }
 
-    sort(v.begin(), v.end());
-    for (int x = 0; x < n; x++) {
-        cout << v[x] << endl;
+    sort(a.begin(), a.end());  // {{1,2},{2,0},{3,1}}
+
+    vector<int> p(n);
+    for (int x=0; x<n; x++) {
+        p[a[x].second] = x; // p[2]=0, p[0]=1, p[1]=2
+    }
+
+    for (int x=0; x<n; x++) {
+        cout << p[x] << " ";
     }
 
     return 0;
