524b97759eb07caef4b218ef3f17cd4237e82fcd
HEEJIN LIM <xeexin0331@gmail.com>
2025-04-18 15:16:45 +0900

    S_5_5648

--- main.cpp
+++ main.cpp
@@ -1,113 +1,31 @@
-// 백준 G_4_17141
+// 백준 S_5_5648
+
+/**
+모든 원소가 양의 정수인 집합이 있을 때, 원소를 거꾸로 뒤집고 그 원소를 오름차순으로 정렬하는 프로그램을 작성하세요.
+단, 원소를 뒤집었을 때 0이 앞에 선행되는 경우는 0을 생략해야합니다.
+*/
 
 #include <iostream>
-#include <queue>
 #include <vector>
-#include<cstring>
+#include <algorithm>
 using namespace std;
-int n, m; // 바이러스 M개
-int arr[51][51]; //. 0은 빈 칸, 1은 벽, 2는 바이러스를 놓을 수 있는 칸
-int visited[51][51];
-bool check[51][51]; //바이러스 놓았는지 확인
-
-struct node {
-    int y, x;
-};
-
-vector<node> virus;
-vector<node> virus_candidate; // 2인 위치 저장
-
-int MIN = 21e8;
-queue<node> q;
-int direct[4][2] = {-1, 0, 1, 0, 0, -1, 0, 1};
-
-
-//모두 퍼졌는지 확인
-bool Spread() {
-    for (int y = 0; y < n; y++) {
-        for (int x = 0; x < n; x++) {
-            if (arr[y][x] == 0 && visited[y][x] == -1) {return false;}
-        }
-    }
-    return true;
-}
-
-
-int bfs() {
-    queue<node> q;
-
-    memset(visited, -1, sizeof(visited));
-
-    for (int i = 0; i < virus.size(); i++) {
-        q.push({virus[i].y, virus[i].x});
-        visited[virus[i].y][virus[i].x] = 0;
-    }
-
-    int res=0;
-
-    while (!q.empty()) {
-        node now = q.front();
-        q.pop();
-
-        for (int t=0; t<4; t++) {
-            int dy = now.y + direct[t][0];
-            int dx = now.x + direct[t][1];
-
-            if (dy<0 || dx<0|| dy>=n|| dx>=n) continue;
-            if (arr[dy][dx] != 1 && visited[dy][dx] == -1) {
-                visited[dy][dx] = visited[now.y][now.x] + 1;
-                res=max(res, visited[dy][dx]);
-                q.push({dy, dx});
-            }
-        }
-    }
-    return Spread() ? res : 21e8; // 실패 시 큰 수 리턴
-}
-
-// 바이러스 위치 조합
-void dfs(int yy, int xx, int lev) {
-    if (lev == m) {
-        int res = bfs();
-        if (Spread() == true) {
-            if (MIN > res) {
-                MIN = res;
-            }
-        }
-        return;
-    }
-
-    for (int y = yy; y < n; y++) {
-        for (int x = xx; x < n; x++) {
-            if (arr[y][x] != 2 || check[y][x] == true) continue;
-
-            check[y][x] = true;
-            virus.push_back({y, x});
-            dfs(y, x, lev + 1);
-            check[y][x] = false;
-            virus.pop_back();
-        }
-
-        yy = 0;
-    }
-}
-
-
 int main() {
-    cin >> n >> m;
+    int n;
+    cin >> n;
+    vector<long long> v;
 
-    for (int y = 0; y < n; y++) {
-        for (int x = 0; x < n; x++) {
-            cin >> arr[y][x];
-        }
+    for (int x = 0; x < n; x++) {
+        string num;
+        cin >> num;
+        reverse(num.begin(), num.end());
+        long long reverseNum = stoll(num);
+        v.push_back(reverseNum);
     }
 
-    dfs(0, 0, 0);
-
-    if (MIN == 21e8) {
-        MIN = -1;
+    sort(v.begin(), v.end());
+    for (int x = 0; x < n; x++) {
+        cout << v[x] << endl;
     }
-    cout << MIN << endl;
-
 
     return 0;
 }
