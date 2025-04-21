dd78705902b1de130606431bbba6d0edeeef98ce
HEEJIN LIM <xeexin0331@gmail.com>
2025-04-21 17:19:30 +0900

    G_5_2170

--- main.cpp
+++ main.cpp
@@ -1,45 +1,53 @@
-// 백준 S_2_108870
+// 백준 G_5_2170
 
 /*
-    Xi를 좌표 압축한 결과 X'i의 값은 Xi > Xj를 만족하는 서로 다른 좌표 Xj의 개수와 같아야 한다.
 
-[2 4 -10 4 -9]
-
--10 -9 2 4 4  -> 정렬
- 2  3  0 3 1  -> 압축 (예. 2보다 작은게 2개, 4보다 작은게 3개, -10보다 작은거 0개...)
 
 
 */
 
 #include <iostream>
-#include <unordered_map>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
 int main() {
+    //속도 향상
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
     int n;
-    cin >> n;
-    vector<int> v(n); // 원본
+    cin >> n; //4
 
-    for (int i = 0; i < n; i++) {
-        cin >> v[i];
+    vector<pair<int, int> > line(n);
+
+    for (int x = 0; x < n; x++) {
+        cin >> line[x].first >> line[x].second;
     }
 
-    vector<int> vv(v); // vv에 v복사
+    sort(line.begin(), line.end()); // 정렬
 
-    sort(vv.begin(), vv.end()); // 오름차순 정렬
 
-    vv.erase(unique(vv.begin(), vv.end()), vv.end()); //중복 제거 + 중복 숫자 뒤로 밀림
+    int st = line[0].first;
+    int ed = line[0].second;
+    int ret = 0;
 
-    unordered_map<int, int> um;
-    for (int x=0; x<vv.size(); x++) {
-        um[vv[x]]=x;
+    for (int x = 1; x < n; x++) {
+        int s = line[x].first;
+        int e = line[x].second;
+
+        if (ed >= s) {
+            ed = max(ed,e); //겹치면 확장
+        } else {
+            ret += (ed - st); // 끊기면 누적
+            st = s;
+            ed = e;
+        }
     }
 
-    for (int x=0; x<n; x++) {
-        cout << um[v[x]] << " ";
-    }
+    ret += (ed - st);
+
+    cout << ret ;
 
     return 0;
 }
