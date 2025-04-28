cadf88753bd9ebcb36a029a4f99612892f9ee1b3
HEEJIN LIM <xeexin0331@gmail.com>
2025-04-28 16:47:03 +0900

    S_2_1654

--- main.cpp
+++ main.cpp
@@ -1,47 +1,49 @@
-// 백준 G_5_2230
+// 백준 S_2_1654
 
 /*
 
+오영식 k개의 랜선 (길이 제각각)
+박성원  N개의 랜선
 
+k개의 랜선으로 n개의 랜선을 만들쟈 (이때 최대 랜선 길이를 구하시오)
 
 */
 
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    int n, m;
-    cin >> n >> m;
+    int k, n;
+    cin >> k >> n; // 4 11
+    int max_len = 0;
 
-    int arr[n];
-    for (int x = 0; x < n; x++) {
-        cin >> arr[x];
+    vector<int> v(k);
+    for (int i = 0; i < k; i++) {
+        cin >> v[i]; // 802 743 457 539
+        max_len = max(max_len, v[i]);
     }
 
-    sort(arr, arr + n); // 정렬 1 3 5
+    long long left = 1; // 0으로 못 나눔!
+    long long right = max_len; //802
+    long long mid = (left + right) / 2; //401
 
-    int MIN = 21e8;
-    int st = 0;
-    int ed = 0;
-
-    while (ed < n) {
-        int diff = arr[ed] - arr[st];
-
-        if (diff < m) {
-            ed++;
-        } else {
-            MIN = min(MIN, diff);
-            st++;
+    while (left <= right) {
+        int cnt = 0;
+        for (int x = 0; x < k; x++) {
+            cnt += v[x] / mid;
         }
+
+        if (cnt >= n) left = mid + 1; //왜냐 최대길이를 구해야 함으로
+        else right = mid - 1;
+        mid = (left + right) / 2;
     }
 
+    cout << mid << endl;
 
-    cout << MIN;
 
     return 0;
 }
