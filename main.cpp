e268a7d9e1fb6643ab92d375a8fadc84b818dcad
HEEJIN LIM <xeexin0331@gmail.com>
2025-04-28 18:18:29 +0900

    S_2_2806 푸는 중

--- main.cpp
+++ main.cpp
@@ -1,56 +1,49 @@
-// 백준 S_4_1920
+// 백준 S_2_2805
 
 /*
-존재하면 1을, 존재하지 않으면 0을 출력
+    나무의 수 N과 상근이가 집으로 가져가려고 하는 나무의 길이 M
 
+    20
+    15
+    10
+    17
+
+    h : 15
+
+    15 -> 5
+    15 -> 0
+    10 -> 0
+    15 -> 2
+
+    m미터 나무를 집에 가져가고 싶은데 그 최댓값을 구해라
 */
 
-#include <algorithm>
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
-int n,m;
-vector<int> v1;
-void binary_search(int target) {
-    int st = 0;
-    int ed = n-1;
-    int mid;
 
-    while (st <= ed) {
-        mid= (st+ed)/2;
-
-        if (v1[mid] == target) {
-            cout << 1 << "\n";
-            return;
-        }else if (v1[mid] > target) {
-            ed = mid - 1;
-        }else {
-            st = mid + 1;
-        }
-    }
-    cout << 0 << "\n";
-}
 int main() {
 
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    cin >>n; //5
-    int k;
-    for(int x=0;x<n;x++) {
-        cin >> k;
-        v1.push_back(k); //4 1 5 2 3
-    }
+    int n,m;
+    cin >> n >> m; // 4 7
 
-    //정렬
-    sort(v1.begin(),v1.end()); // 1 2 3 4 5
+    // 적어도 m미터
 
-    cin>>m; //5
-    int target;
-    for (int x=0; x<m; x++) {
-        cin >> target;
-        binary_search(target);
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i]; // 20 15 10 17
     }
+    sort(v.begin(), v.end()); // 10 15 17 20
+
+    long long st = 0;
+    long long ed =  n-1;
+
+
+
 
     return 0;
 }
