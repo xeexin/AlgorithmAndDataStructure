708b89acd10efb8d30a1af4c4088579610f277ae
Lim Hee Jin <xeexin0331@gmail.com>
2025-04-29 22:51:22 +0900

    S_2_2805

--- main.cpp
+++ main.cpp
@@ -27,9 +27,11 @@ int main() {
 
     ios::sync_with_stdio(false);
     cin.tie(0);
+    cout.tie(0);
 
     int n,m;
     cin >> n >> m; // 4 7
+    int MAX=0;
 
     // 적어도 m미터
 
@@ -40,10 +42,25 @@ int main() {
     sort(v.begin(), v.end()); // 10 15 17 20
 
     long long st = 0;
-    long long ed =  n-1;
+    long long ed = v[n - 1];
 
+    while(st<=ed) {
+        long long sum = 0;
+        long long mid = (st + ed) / 2;
 
+        for(int x=0; x<n; x++){
+            if(v[x]-mid > 0 ) sum += (v[x] - mid);
+        }
 
+        if(sum >= m){
+            MAX = mid;
+            st = mid+1;
+        }else{
+            ed = mid-1;
+        }
+    }
+
+    cout << MAX;
 
     return 0;
 }
