d38e28212a5b1139237321ca496288be3ad8263a
HEEJIN LIM <xeexin0331@gmail.com>
2025-05-01 10:36:39 +0900

    S_2_16401

--- main.cpp
+++ main.cpp
@@ -1,21 +1,7 @@
-// 백준 S_2_2805
+// 백준 S_2_16401
 
 /*
-    나무의 수 N과 상근이가 집으로 가져가려고 하는 나무의 길이 M
-
-    20
-    15
-    10
-    17
-
-    h : 15
-
-    15 -> 5
-    15 -> 0
-    10 -> 0
-    15 -> 2
-
-    m미터 나무를 집에 가져가고 싶은데 그 최댓값을 구해라
+    M명의 조카가 있고 N개의 과자가 있을 때, 조카 1명에게 줄 수 있는 막대 과자의 최대 길이를 구하라.
 */
 
 #include <iostream>
@@ -24,43 +10,37 @@
 using namespace std;
 
 int main() {
-
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-
-    int n,m;
-    cin >> n >> m; // 4 7
-    int MAX=0;
-
-    // 적어도 m미터
-
-    vector<int> v(n);
+    int m, n;
+    cin >> m >> n; // 3 10
+    vector<int> snack(n);
     for (int i = 0; i < n; i++) {
-        cin >> v[i]; // 20 15 10 17
+        cin >> snack[i]; //1 2 3 4 5 6 7 8 9 10
     }
-    sort(v.begin(), v.end()); // 10 15 17 20
+    sort(snack.begin(), snack.end());
 
-    long long st = 0;
-    long long ed = v[n - 1];
+    int st = 1;
+    int ed = snack[n - 1];
 
-    while(st<=ed) {
-        long long sum = 0;
-        long long mid = (st + ed) / 2;
+    int ret = 0;
 
-        for(int x=0; x<n; x++){
-            if(v[x]-mid > 0 ) sum += (v[x] - mid);
+    while (st <= ed) {
+        int mid = (st + ed) / 2;
+
+        int cnt = 0;
+        for (int x = 0; x < n; x++) {
+            cnt += snack[x] / mid;
         }
-
-        if(sum >= m){
-            MAX = mid;
-            st = mid+1;
-        }else{
-            ed = mid-1;
+        if (cnt < m) {
+            ed = mid - 1;
+        } else {
+            ret = mid;
+            st = mid + 1;
         }
     }
-
-    cout << MAX;
+    cout << ret;
 
     return 0;
 }
