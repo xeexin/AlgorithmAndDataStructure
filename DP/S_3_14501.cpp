d698b3dd0861964177f6d2f931462ca219eb0c1a
Lim Hee Jin <xeexin0331@gmail.com>
2025-05-12 22:53:19 +0900

    S_3_14501

--- DP/S_3_14501.cpp
+++ DP/S_3_14501.cpp
@@ -7,10 +7,36 @@
 #include<algorithm>
 #include<vector>
 using namespace std;
+// n <= 15
+int dp[16];  // dp[x] = x일 부터의 최대 수익
 int main(){
 
+    // 속도향상
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
 
- cout << "Hello World!" << endl;
+    //입력
+    int n;
+    cin >> n;
+
+    vector<pair<int,int> >v(n);
+    for(int x=0; x<n; x++){
+        cin >> v[x].first;
+        cin >> v[x].second;
+    }
+
+    // 2. 풀이
+    for(int x=n-1; x>=0; x--){
+        int time = v[x].first;
+        int profit = v[x].second;
+
+        if(x+time <= n) dp[x] = max(dp[x+1], profit + dp[x+time]);
+        else dp[x] = dp[x+1];
+    }
+
+    cout << dp[0];
+
   return 0;
 
 }
